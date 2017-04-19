35a30dceb23773685d373e92f54b2f9651562f0c
Daniela <danii.matallana@gmail.com>
2017-04-19 08:07:31 -0500

    Lectura del acelerometro en arduino

---
+++ acelerometro/acelerometro.ino
@@ -0,0 +1,29 @@
+const int xPin=A4;
+const int yPin=A3;
+const int zPin=A2;
+//int delay=500;
+void setup()
+{
+ Serial.begin(9600);
+ analogReference(EXTERNAL);
+ pinMode(xPin,INPUT);
+ pinMode(yPin,INPUT);
+ pinMode(zPin,INPUT);
+}
+
+void loop() 
+{
+   Serial.print(analogRead(xPin));
+   Serial.print(":");
+   
+
+   
+   Serial.print(analogRead(yPin));
+   Serial.print(":");
+   
+   
+  
+   Serial.println(analogRead(zPin));
+   
+}
+
